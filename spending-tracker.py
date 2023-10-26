f458f2edeed675cd9c8eab066bdce35648160e97
bsh86 <bsh86@uclive.ac.nz>
2023-10-27 10:03:21 +1300

    created main project file.

---
+++ spending-tracker.py
@@ -0,0 +1,18 @@
+"""
+This is the main file for my spending tracker app.
+
+@author Ben Shirley
+@date 27 Oct 2023
+"""
+
+
+def main():
+    """Main function for my code"""
+    pass
+
+def analyse_csv_data(filename):
+    """function that takes in the name of a csv file and returns
+    """
+    
+if __name__ == "__main__":
+    main()
