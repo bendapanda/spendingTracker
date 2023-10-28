12115f758eade5e336a0d289dd985d3c011f7964
bsh86 <bsh86@uclive.ac.nz>
2023-10-28 22:38:05 +1300

    split main file into different subfiles

---
+++ classification.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from transaction_structure import *
+
+def encode_data_for_learning(data: pd.DataFrame) -> pd.DataFrame:
+    """prepares the given data for learning by normalising it, removing unwanted attributes, and one-hot encoding""" 
+
+    dates = data[HEADER.DATE]
+    codes = data[HEADER.CODE]
+    foreign = data[HEADER.FOREIGN]
+    conversion = data[HEADER.CONVERSION_COST]
+
+    chopped_data = data.drop([HEADER.DATE, HEADER.CODE, HEADER.FOREIGN, HEADER.CONVERSION_COST, HEADER.REF], axis=1, inplace=False)
+    #one-hot encoding:
+    spending_types = chopped_data[HEADER.SPEND_TYPE].unique()
+    locations = chopped_data[HEADER.LOCATION].unique()
+    particulars = chopped_data[HEADER.PARTICULARS].unique()
+
+    encoded_data = pd.get_dummies(chopped_data, columns=[HEADER.SPEND_TYPE, HEADER.LOCATION, HEADER.PARTICULARS])
+    encoded_data[HEADER.QUANTITY] = (encoded_data[HEADER.QUANTITY] - encoded_data[HEADER.QUANTITY].min()) / (encoded_data[HEADER.QUANTITY].max()-encoded_data[HEADER.QUANTITY].min())
+    
+    return encoded_data
+def elbow_method_for_number_clusters(data):
+    """graphs the overall cost of the k-prototype clustering varying values of k to 
+    see which number of clusters fits the data best"""
+    cost_values = []
+    k=1
+    old_cost = np.inf
+    new_cost = 1000
+    while old_cost / new_cost > 1.01:
+        old_cost = new_cost
+        new_data, new_cost = perform_k_prototypes_clustering(data, k)
+        cost_values.append(new_cost)
+        k += 1
+    
+    #plot the data
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(1, len(cost_values)+1), cost_values, marker='o', linestyle='--', color='b')
+    plt.xlabel('Number of Clusters (k)')
+    plt.ylabel('cost')
+    plt.title('Elbow Method for Optimal k (K-Prototypes)')
+    plt.grid(True)
+    plt.show()
+
+def perform_k_prototypes_clustering(data: pd.DataFrame, k:int) -> pd.DataFrame:
+    """performs k-prototypes clustering, which is a combination of k-means for the numerical data, and
+    k-modes for the catagorical data
+    """
+    encoded_data = encode_data_for_learning(data)
+    # currently the only numerical data is the amount
+    k_proto = KPrototypes(n_clusters=k, init="Cao", verbose=2)
+
+    clusters = k_proto.fit_predict(encoded_data, categorical=list(range(1, len(encoded_data.columns))))
+    data["classification"] = clusters
+    return data, k_proto.cost_
+
+def perform_k_means_clustering(data: pd.DataFrame, k: int) -> pd.DataFrame:
+    """
+    performs k-means clusteringing on the data to provide each entry a catagory based on similarity.
+
+    parameters:
+        Data: data to be catagorised
+        k: the number of clusters to be formed
+    """
+
+    # Step one: reduce data to be entirely numberical
+    # In the data set, we have all of the variables in HEADER to consider
+    # However, date likely will not affect the data
+    # Try: using number for date, or day of week
+    # For now I will ignore it
+    # For now normalise linearly, but investigate normal?
+   
+    encoded_data = encode_data_for_learning(data)
+    # Step two: randomly allocate k-means in the search space
+    #k_means = KMeans(n_clusters=10)
+    #k_means.fit(encoded_data)
+    #cluster_assignments = k_means.labels_
+
+
+    
+    cluster_assignments, inertia = k_means_no_library(encoded_data, k)
+        
+
+    
+    # Step 4: add the catagories to the original dataset
+    data["classification"] = cluster_assignments
+    return data, inertia
+
+def k_means_no_library(encoded_data: pd.DataFrame, k: int) -> pd.Series:
+    """This is my own implementation of the k-means algorithm. It needs a tidy up but I wanted to see if i could 
+    do it without a library"""
+    vector_dimension = encoded_data.shape[1]
+    k_means = {tuple(np.random.rand(vector_dimension)): [] for i in range(k)}
+    
+   
+    last_error = np.inf
+    error = 0
+    TARGET = 0.0001
+    # Step three: assign each point to the closest mean, and then move that mean to the average of all the points it is assigned to
+    while(abs(error - last_error) > TARGET):
+        last_error = error
+        error = 0
+        for id, data_point in encoded_data.iterrows(): # go through and assign all points to their closest neigbour
+            min_distance = np.inf
+            current_closest = None
+            for vector in k_means.keys():
+                k_vector = np.array(vector)
+                new_dist = np.sum((k_vector - data_point) ** 2) 
+                if new_dist < min_distance:
+                    min_distance = new_dist
+                    current_closest = vector
+                print(min_distance)
+            k_means[current_closest].append(data_point)
+            error += min_distance
+        new_means = {}
+        for vector, elements in k_means.items():
+            if len(elements) > 0:
+                new_k = np.sum(np.array(elements), axis=0) / len(elements)
+            else:
+                new_k = vector
+            new_means[tuple(new_k)] = []
+        k_means = new_means
+
+    result = []
+    k_means = list(k_means.keys())
+    print(len(k_means))
+    for id, data_point in encoded_data.iterrows():
+        min_distance = np.inf
+        current_closest = None
+        for i in range(len(k_means)):
+            k_vector = np.array(k_means[i])
+            new_dist =  np.sum((k_vector - data_point) ** 2)
+            if new_dist < min_distance:
+                    min_distance = new_dist
+                    current_closest = i
+        result.append(current_closest)
+    return result, last_error
--- spending-tracker.py
+++ spending-tracker.py
@@ -15,20 +15,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
+from kmodes.kprototypes import KPrototypes
 
+from transaction_structure import *
+from classification import *
 FILENAME = "transactions-year.csv"
-
-class HEADER :
-    SPEND_TYPE = "Type"
-    LOCATION =  "Details"
-    PARTICULARS = "Particulars"
-    DATE = "Date"
-    CODE =  "Code"
-    REF =  "Reference"
-    QUANTITY =  "Amount"
-    FOREIGN = "ForeignCurrencyAmount"
-    CONVERSION_COST=  "ConversionCharge"
-
 
 PERSONAL_SPENDING_TYPES = ["Rent", "Bills", "Flat things", "Groceries", "work payments", ""]
 
@@ -43,16 +34,33 @@ def main():
     general_spending = data.loc[data[HEADER.SPEND_TYPE] == "Visa Purchase"]
     general_spending_per_day = get_total_spending_per_day(general_spending)
 
-    print(prompt_for_spending_types(data).head(50))
+    #elbow_method_for_number_clusters(data)
+    classified_data = prompt_for_spending_types(data)
+    print(classified_data.head(50))
+    classified_data.to_csv("classified_dataset.csv")
+    #for classification in classified_data["classification"].unique():
+    #    classified_data[classified_data["classification"] == classification, "Amount"].plot(kind="hist", figsize=(10,8), label=classification, legend=True)
+    #plt.show()
+
 
 
 def prompt_for_spending_types(data):
     """ask user to list the types of things they spend money on.
-    then perfroms k-means classification and asks the user to assign them """
+    then perfroms k-means classification and asks the user to assign them 
+
+    parameters:
+        data: the data to be classified
+
+    returns:
+        new_data, the data together with a labeled classification column
+    
+    TODO: figure out how many catagories the data should be split into automatically
+    TODO: add invalid input catching
+    """
     print("Label the catagories of things that you spend money on (type q to stop):\n")
     catagories = []
     
-    new_data, inertia = perform_k_means_clustering(data, 10)
+    new_data, inertia = perform_k_prototypes_clustering(data, 6)
 
     mapping = {}
     for catagory in new_data["classification"].unique():
@@ -77,106 +85,28 @@ def prompt_for_spending_types(data):
 
 
 def get_spending_types(data):
-    """returns a 1-d numpy array containing the unique types of spending that occured"""
+    """
+    gets the types of spending that are present in the data
+
+    parameters:
+        data: the data to be analysed
+    returns:
+         a 1-d numpy array containing the unique types of spending that occured
+    """
     return data[HEADER.SPEND_TYPE].unique()
 
 def get_total_spending_per_day(data):
-    """returns a new dataset of the form date, quantity which describes how much was spent on any set day"""
+    """
+    returns the total amount spent on each day
+    parameters:
+        data: The data to be analysed
+    returns:
+        a new dataset of the form date, quantity which describes how much was spent on any set day"""
     daily_spendings = data.groupby(HEADER.DATE)[HEADER.QUANTITY].sum().to_frame()
     daily_spendings.index.name = HEADER.DATE
     daily_spendings.reset_index(inplace=True)
     return daily_spendings
 
-def perform_k_means_clustering(data, k):
-    """performs k-means clusteringing on the data to provide each entry a catagory based on similarity.
-    """
-
-    # Step one: reduce data to be entirely numberical
-    # In the data set, we have all of the variables in HEADER to consider
-    # However, date likely will not affect the data
-    # Try: using number for date, or day of week
-    # For now I will ignore it
-
-    dates = data[HEADER.DATE]
-    codes = data[HEADER.CODE]
-    foreign = data[HEADER.FOREIGN]
-    conversion = data[HEADER.CONVERSION_COST]
-
-    chopped_data = data.drop([HEADER.DATE, HEADER.CODE, HEADER.FOREIGN, HEADER.CONVERSION_COST, HEADER.REF], axis=1, inplace=False)
-    #one-hot encoding:
-    spending_types = chopped_data[HEADER.SPEND_TYPE].unique()
-    locations = chopped_data[HEADER.LOCATION].unique()
-    particulars = chopped_data[HEADER.PARTICULARS].unique()
-
-    encoded_data = pd.get_dummies(chopped_data, columns=[HEADER.SPEND_TYPE, HEADER.LOCATION, HEADER.PARTICULARS])
-    encoded_data[HEADER.QUANTITY] = (encoded_data[HEADER.QUANTITY] - encoded_data[HEADER.QUANTITY].min()) / (encoded_data[HEADER.QUANTITY].max()-encoded_data[HEADER.QUANTITY].min())
-    # For now normalise linearly, but investigate normal?
-    
-
-    # Step two: randomly allocate k-means in the search space
-    k_means = KMeans(n_clusters=10)
-    k_means.fit(encoded_data)
-    cluster_assignments = k_means.labels_
-
-
-    
-    #result, inertia = k_means_no_library(encoded_data, k)
-        
-
-    
-    # Step 4: add the catagories to the original dataset
-    data["classification"] = cluster_assignments
-    return data, k_means.inertia_
-
-def k_means_no_library(encoded_data, k):
-    """This is my own implementation of the k-means algorithm. It needs a tidy up but I wanted to see if i could 
-    do it without a library"""
-    vector_dimension = encoded_data.shape[1]
-    k_means = {tuple(np.random.rand(vector_dimension)): [] for i in range(k)}
-    
-   
-    last_error = np.inf
-    error = 0
-    TARGET = 0.0001
-    # Step three: assign each point to the closest mean, and then move that mean to the average of all the points it is assigned to
-    while(abs(error - last_error) > TARGET):
-        last_error = error
-        error = 0
-        for id, data_point in encoded_data.iterrows(): # go through and assign all points to their closest neigbour
-            min_distance = np.inf
-            current_closest = None
-            for vector in k_means.keys():
-                k_vector = np.array(vector)
-                new_dist = np.sum((k_vector * data_point) ** 2) 
-                if new_dist < min_distance:
-                    min_distance = new_dist
-                    current_closest = vector
-            k_means[current_closest].append(data_point)
-            error += min_distance
-        new_means = {}
-        for vector, elements in k_means.items():
-            if len(elements) > 0:
-                new_k = np.sum(np.array(elements), axis=0) / len(elements)
-            else:
-                new_k = vector
-            new_means[tuple(new_k)] = []
-        k_means = new_means
-        print(error, last_error)
-
-    result = []
-    k_means = list(k_means.keys())
-    for id, data_point in encoded_data.iterrows():
-        min_distance = np.inf
-        current_closest = None
-        for i in range(len(k_means)):
-            print(len(k_means))
-            k_vector = np.array(k_means[i])
-            new_dist =  np.sum((k_vector * data_point) ** 2)
-            if new_dist < min_distance:
-                    min_distance = new_dist
-                    current_closest = i
-        result.append(current_closest)
-    return result, last_error
 
 def plot_spending_by_time(data):
     """
